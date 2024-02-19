cb9da15c030ac7106467ed386ec1cd93d4a0963f
MABIRUS <killing.ak56@gmail.com>
2024-02-19 21:35:57 +0300

    first design

---
+++ PhotoEditor Mini/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace PhotoEditor_Mini
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image files|*.png; *.jpg";
+            bool? state = ofd.ShowDialog();
+
+            if (state == true)
+            {
+                Uri path = new Uri(ofd.FileName);
+                cnvsImage.Source = new BitmapImage(path);
+            }
+        }
+
+        private void btnDel_Click(object sender, RoutedEventArgs e) => cnvsImage.Source = null;
+    }
+}
